e9adfca90be6af2c3455a3e64be6d755f8fe886b
lgiacalo <lgiacalo@student.42.fr>
2020-10-07 10:55:28 +0200

    wip add comm

---
+++ src/commentaire.dev.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var moment = require('moment'); // require
+// const form_commentaire = document.getElementById("form_comm");
+
+
+var btnFormComm = document.querySelector("#form_comm button");
+var textarea = document.getElementById("id_textarea");
+var inputName = document.getElementById("id_input_name");
+var inputMail = document.getElementById("id_input_mail");
+var templateComm = document.getElementById("template_commentary");
+console.log('btnFormComm :>> ', btnFormComm);
+var all_comms = document.getElementById("all_commentaries");
+console.log('all_comms :>> ', all_comms);
+btnFormComm.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (textarea.value.trim() === "" || inputName.value.trim() === "" || inputMail.value.trim() === "") return;
+  var newComm = createNewComm();
+  all_comms.appendChild(newComm);
+});
+
+function createNewComm() {
+  console.log(textarea.value);
+  console.log(inputName.value);
+  console.log(inputMail.value);
+  var newComm = templateComm.cloneNode(true);
+  console.log(newComm);
+  console.log('newComm.id :>> ', newComm.id);
+  newComm.id = "";
+  console.log('newComm.id :>> ', newComm.id);
+  var newh3 = newComm.querySelector("h3");
+  console.log('newh3 :>> ', newh3);
+  newh3.innerHTML = inputName.value;
+  var newDate = newComm.querySelector("em");
+  newDate.innerHTML = " - le ".concat(moment().format('DD MMM YYYY à hh:mm'));
+  console.log('newDate :>> ', newDate);
+  var newp = newComm.querySelector("p");
+  console.log('newp :>> ', newp);
+  newp.innerHTML = textarea.value;
+  textarea.value = "";
+  inputName.value = "";
+  inputMail.value = "";
+  return newComm;
+}
---
+++ src/commentaire.js
@@ -0,0 +1,69 @@
+
+var moment = require('moment'); // require
+
+// const form_commentaire = document.getElementById("form_comm");
+const btnFormComm = document.querySelector("#form_comm button");
+const textarea = document.getElementById("id_textarea");
+const inputName = document.getElementById("id_input_name");
+const inputMail = document.getElementById("id_input_mail");
+
+const templateComm = document.getElementById("template_commentary");
+console.log('btnFormComm :>> ', btnFormComm);
+
+const all_comms = document.getElementById("all_commentaries");
+console.log('all_comms :>> ', all_comms);
+
+btnFormComm.addEventListener("click", (e) => {
+    e.preventDefault();
+
+
+    if (textarea.value.trim() === "" || inputName.value.trim() === "" || inputMail.value.trim() === "")
+        return;
+
+
+    const newComm = createNewComm();
+    all_comms.appendChild(newComm);
+})
+
+
+
+
+
+
+
+
+
+function createNewComm(){
+
+    console.log(textarea.value);
+    console.log(inputName.value);
+    console.log(inputMail.value);
+
+    const newComm = templateComm.cloneNode(true);
+    console.log(newComm);
+    
+    console.log('newComm.id :>> ', newComm.id);
+    newComm.id = "";
+    console.log('newComm.id :>> ', newComm.id);
+
+
+    const newh3 = newComm.querySelector("h3");
+    console.log('newh3 :>> ', newh3);
+    newh3.innerHTML = inputName.value;
+    
+    const newDate = newComm.querySelector("em");
+    newDate.innerHTML = ` - le ${moment().format('DD MMM YYYY à hh:mm')}`;
+    console.log('newDate :>> ', newDate);
+    
+    const newp = newComm.querySelector("p");
+    console.log('newp :>> ', newp);
+    newp.innerHTML = textarea.value;
+    
+    
+    
+    textarea.value = "";
+    inputName.value = "";
+    inputMail.value = "";
+
+    return newComm;
+}
