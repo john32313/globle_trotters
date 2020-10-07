58179723663de152fce55a1dfdfd0b624b9d0ffb
john32313 <francois.jonathan13@gmail.com>
2020-10-07 17:54:45 +0200

    carousel + pagination ok

--- src/caroussel.js
+++ src/caroussel.js
@@ -3,25 +3,34 @@
   const arrImgOfCaroussel = ['../Image/caroussel/img1.jpg', '../Image/caroussel/img2.jpg', '../Image/caroussel/img3.jpg', '../Image/caroussel/img4.jpg', '../Image/caroussel/img5.jpg', '../Image/caroussel/img6.jpg'];
 
   let index = 0;
-  imageAppend(caroussel,(setImg(arrImgOfCaroussel[index])));
-  
+  imageAppend(caroussel, (setImg(arrImgOfCaroussel[index])));
+  pagination(index, arrImgOfCaroussel, caroussel)
+
+  const paginationEl = document.querySelector('.pagination');
+      
   const right = document.querySelector(".caroussel_right");
   const left = document.querySelector(".caroussel_left");
   let timer = setInterval(()=>{
     index = changeImage('right', index, arrImgOfCaroussel, caroussel);
-  },1500);
+  },3000);
   left.addEventListener('click',e=>{
     clearInterval(timer);
     e.preventDefault();
-    console.log('left');
     index = changeImage('left', index, arrImgOfCaroussel, caroussel);
   });
   right.addEventListener('click', e => {
     clearInterval(timer);
     e.preventDefault();
-    console.log('right');
     index = changeImage('right', index, arrImgOfCaroussel, caroussel);
   });
+  for (let i = 0; i < arrImgOfCaroussel.length; i++) {
+    paginationEl.children[i].addEventListener('click', e => {
+      clearInterval(timer);
+      e.preventDefault();
+      changeImage('number', i, arrImgOfCaroussel, caroussel)
+      console.log(i)
+    });
+  };
 })()
 
 /**
@@ -50,12 +59,18 @@ function changeImage(direction, index, arrImg, contain) {
     case 'left':
       index===0 ? index = arrImg.length-1 : index--;
       imageAppend(contain, setImg(arrImg[index]));
+      pagination(index);
       return index;
       break;
     case 'right':
       index === arrImg.length - 1 ? index = 0 : index++;
       imageAppend(contain, setImg(arrImg[index]));
+      pagination(index);
       return index;
+      break;
+    case 'number':
+      imageAppend(contain, setImg(arrImg[index]));
+      pagination(index);
       break;
     default:
       console.error('wrong parameters direction');
@@ -67,9 +82,18 @@ function changeImage(direction, index, arrImg, contain) {
  * @param {Object} img img to append in carousel
  */
 function imageAppend(contain, img) {
-
-  console.log(typeof contain)
   contain.removeChild(contain.lastChild);
   contain.appendChild(img);
+}
+/**
+ * @param {number} index index of pag button to on
+ */
+function pagination(index) {
+  const pagination = document.querySelector('.pagination');
+  for(let i=0; i<6; i++) {
+    if (pagination.children[i].classList.contains('pag_on')) pagination.children[i].classList.remove('pag_on');
+    
+  }
+  pagination.children[index].classList.add('pag_on')
 }
 
