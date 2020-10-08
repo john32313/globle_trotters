543216f12da4e55451e788fed6602429fd0c6164
john32313 <francois.jonathan13@gmail.com>
2020-10-08 10:06:45 +0200

    navbar close responsive js

--- main.js
+++ main.js
@@ -9,3 +9,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//navbar mouseOut
+window.addEventListener('mouseup', e => {
+    let navi = document.querySelector('.navbar')
+    let nav = e.composedPath();
+    let check = document.querySelector('#menu_button')
+    if (!nav.includes(navi)) {
+        check.checked = false;
+    }
+
+});
