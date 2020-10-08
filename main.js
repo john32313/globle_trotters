28aa44f516cefcdd7a8cc128c6cfda736ad5c6f2
lgiacalo <lgiacalo@student.42.fr>
2020-10-08 11:02:58 +0200

    arrow scroll index ok

--- main.js
+++ main.js
@@ -9,3 +9,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const arrowScroll = $("#arrow_scroll");
+arrowScroll.fadeOut(1);
+let isScrolling;
+
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+        if (this.scrollY >= 120) {    
+            arrowScroll.fadeIn(200);
+        } else {
+            arrowScroll.fadeOut(200); 
+        }
+	}, 40);
+}, false);
+
